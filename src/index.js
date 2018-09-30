dd2334be125e38febd6e7da7177d69d9796d2758
nikhinrajkk <nikhinrajkk@gmail.com>
2018-09-30 15:12:21 +0530

    First commit - Added files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from "react-dom";
+import Switch from "./lib/switch";
+
+const App = () => (
+  <Switch />
+);
+
+render(<App />, document.getElementById("root"));
---
+++ src/lib/switch/switch.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+class Switch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default Switch;
