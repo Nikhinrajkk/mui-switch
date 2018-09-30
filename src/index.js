80ab7bc41c639ecc1b1668889194b7c96fd28f65
nikhinrajkk <nikhinrajkk@gmail.com>
2018-09-30 17:18:43 +0530

    Added switch compnent

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import { render } from "react-dom";
 import Switch from "./lib/switch";
 
 const App = () => (
-  <Switch />
+  <div style={{ display: 'flex', margin: 10 }}>
+    <Switch color="yellow" width={40} height={20}/>
+    <Switch color="green" width={100} height={50}/>
+    <Switch color="red" width={200} height={100}/>
+    <Switch color="blue" width={500} height={250}/>
+  </div>
 );
 
 render(<App />, document.getElementById("root"));
--- src/lib/switch/switch.jsx
+++ src/lib/switch/switch.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
-import './styles.css';
+// import './styles.css';
 
 class Switch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      selected: false
     };
   }
 
+  onSwitchClick = () => {
+    debugger;
+    const { selected } = this.state;
+    this.setState({
+      selected: !selected
+    });
+  }
+
   render() {
+    const { width = 40, height = 20, color = 'green' } = this.props;
+    const { selected } = this.state;
+    const style = {
+      width,
+      height,
+      borderWidth: height / 10,
+      borderColor: selected ? color : 'lightGrey',
+      borderStyle: 'solid',
+      borderRadius: height,
+      backgroundColor: selected ? color : 'lightGrey',
+      cursor: 'pointer'
+    };
+    const switchColor = {
+      width: height,
+      height,
+      float: selected ? 'right' : 'left',
+      backgroundColor: 'white',
+      borderRadius: '100%'
+    };
     return (
-      <div>
-        Hello World
-      </div>
+      <div
+        className="switch-wrapper"
+        style={style}
+        role="presentation"
+        onClick={() => this.onSwitchClick()}
+      >
+        <div
+          className="switch"
+          style={switchColor}
+        />
+      </div >
     );
   }
 }
